5609014cc36be36f012221ef2ded794161c3cbf0
cooperlin <cooperlin@tencent.com>
2022-06-04 16:02:17 +0800

    update

--- src/3.model_loading/1.model_loading/model_loading.cpp
+++ src/3.model_loading/1.model_loading/model_loading.cpp
@@ -122,6 +122,7 @@ int main()
         // render the loaded model
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+        // 模型比较大，可适当缩小
         model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));    // it's a bit too big for our scene, so scale it down
         ourShader.setMat4("model", model);
         
